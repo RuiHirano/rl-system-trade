acd09c6b17f41cf4c19eb35ae5b6144410fec184
RuiHirano <r.hrn.0930@gmail.com>
2020-05-09 17:20:56 +0900

    add docker shell

--- src/breakout/dqn/run.sh
+++ src/breakout/dqn/run.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-docker run --rm -it -v ./:./workspace dqn/breakout:latest
+docker run --rm -it -v ~/workspace/rl-system-trade/src/breakout/dqn/result/:/workspace/result/ dqn/breakout:latest
